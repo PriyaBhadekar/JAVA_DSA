d062daaeeb90164c8b01444fc18ab8bb6eb3af9b
PriyaBhadekar <priyabhadekar05@gmail.com>
2025-07-05 18:48:00 +0530

    DSA patter Sliding Window - Problem 219. Contains Duplicate II

---
+++ Sliding_Window/Contains_Duplicates_Two.java
@@ -0,0 +1,32 @@
+package Sliding_Window;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Contains_Duplicates_Two {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        Set<Integer> windows = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(windows.contains(nums[i])){
+                return true;
+            }
+
+            windows.add(nums[i]);
+
+            if(windows.size() > k){
+                windows.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,1};
+        boolean result = containsNearbyDuplicate(nums , 3);
+        System.out.println(result);
+    }
+}
