36cdd2c42a93e74b3a3fdbe30ba9052dbbc5f253
PriyaBhadekar <priyabhadekar05@gmail.com>
2025-11-22 12:23:21 +0530

    Added a simple code that print number both increasing and decreasing way using recursion

---
+++ Recursion/PracticeOneEasyRecursion.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class PracticeOneEasyRecursion {
+    public static void main(String[] args) {
+        
+        //fun(5);
+        funReverse(5);
+    }
+
+    static void fun(int n){
+
+        if(n == 0){
+            return;
+        }
+
+        System.out.println(n);
+        fun(n - 1);;
+    }
+
+    static void funReverse(int n){
+        if(n == 0){
+            return;
+        }
+
+        funReverse(n - 1);
+        System.out.println(n);
+    }
+}
